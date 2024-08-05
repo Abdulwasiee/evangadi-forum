aa927569667dc9bb5e7c40d6253251889c2ceeba
Abdulwasiee <wasungncom@gmail.com>
2024-08-05 14:48:19 +0300

    updated

--- backend/middlewire/auth.js
+++ backend/middlewire/auth.js
@@ -5,12 +5,14 @@ const JWT_SECRET = process.env.SECRET_KEY;
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers["authorization"];
+
   const token =
     authHeader && authHeader.startsWith("Bearer ")
       ? authHeader.split(" ")[1]
       : null;
 
   if (!token) {
+    console.error("No token provided");
     return res.status(401).json({ msg: "No token provided" });
   }
 
@@ -19,6 +21,7 @@ const authMiddleware = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (err) {
+    console.error("Token verification error:", err.message);
     res.status(401).json({ msg: "Invalid token" });
   }
 };
